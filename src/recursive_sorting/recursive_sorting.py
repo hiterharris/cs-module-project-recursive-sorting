10f140b5d3d2756cab15c2db52825a70513e0dcd
Hiter Harris <hiterharris@comcast.net>
2020-05-19 12:48:52 -0400

    completed merge_in_place, merge_sort_in_place

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -47,16 +47,52 @@ def merge_sort(arr):
 
 
 # implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
-    # Your code here
-
-
+def merge_in_place(arr, start, mid, end): 
+    start2 = mid + 1
+  
+    # If the direct merge is already sorted 
+    if (arr[mid] <= arr[start2]): 
+        return
+      
+    # Two pointers to maintain start 
+    # of both arrays to merge 
+    while (start <= mid and start2 <= end): 
+  
+        # If element 1 is in right place 
+        if (arr[start] <= arr[start2]): 
+            start += 1
+        else: 
+            value = arr[start2]
+            index = start2
+  
+            # Shift all the elements between element 1 
+            # element 2, right by 1. 
+            while (index != start): 
+                arr[index] = arr[index - 1]
+                index -= 1
+              
+            arr[start] = value
+  
+            # Update all the pointers 
+            start += 1
+            mid += 1
+            start2 += 1
+    
     return arr
+          
 
-
-def merge_sort_in_place(arr, l, r):
-    # Your code here
-
+def merge_sort_in_place(arr, l, r): 
+    if (l < r): 
+  
+        # Same as (l + r) / 2, but avoids overflow 
+        # for large l and r 
+        m = l + (r - l) // 2
+  
+        # Sort first and second halves 
+        merge_sort_in_place(arr, l, m)
+        merge_sort_in_place(arr, m + 1, r)
+  
+        merge_in_place(arr, l, m, r)
 
     return arr
 
